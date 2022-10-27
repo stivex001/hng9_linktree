7745f58f48dd10663658945a5e41ab8a0906cced
stivex001 <stephenadeyemo@gmail.com>
2022-10-27 14:42:08 +0100

    setting up the projects and folder structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <div className="">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import icon from '../assets/sharebutton.svg'
+
+const Home = () => {
+  return (
+    <div className='w-full h-full '>
+        <div className='mt-[44px] '>
+            <img className='mt-[44px] bg-black' src={icon} alt="" />
+        </div>
+        
+    </div>
+  )
+}
+
+export default Home
